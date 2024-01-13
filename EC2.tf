9eb6e77a28ffaddd1a6b72c21f4498e24f51bd01
vamsi <scvamsi@gmail.com>
2024-01-12 20:02:44 -0500

    EC2.tf

---
+++ EC2.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "devops-QA" {
+ ami = var.ami_id
+ instance_type = "t2.micro"
+
+ tags = {
+    Name = "DevOps-QA"
+ }
+
+}
+
+output "instance_id" {
+   description = "The ec2 instance id"
+   #value = aws_instance.devops.id
+   value = aws_instance.devops-QA.arn
+}
+output "public_ip" {
+   description = "The ec2 instance id"
+   #value = aws_instance.devops.id
+
+   value = aws_instance.devops-QA.public_ip
+}
